20d13bc5e694a29cc0d91ed47afff02fa3f3f2d7
tronghuy2807 <tronghuy2807@gmail.com>
2016-12-25 15:53:14 +0700

    update authen

--- routes/index.js
+++ routes/index.js
@@ -5,74 +5,90 @@ var router = express.Router();
 
 
 router.get('/home', function (req, res) {
-  res.render('index', { user : req.user });
+    if(req.user){
+        res.render('index', {user: req.user});
+    } else {
+        res.render('login');
+    }
 });
 router.get('/', function (req, res) {
-  res.render('login', { user : req.user });
+    res.render('login', {user: req.user});
 });
 
-router.get('/register', function(req, res) {
-  res.render('register', { });
+router.get('/register', function (req, res) {
+    res.render('register', {});
 });
 
-router.post('/register', function(req, res) {
-  if(req.body.password == req.body.confirmPass) {
-    Account.register(new Account({
-      username: req.body.username,
-      fullname: req.body.fullname,
-      email: req.body.email,
-      phone: req.body.phone,
-      leaderPhone: req.body.leaderPhone
-    }), req.body.password, function (err, account) {
-      if (err) {
-        return res.render('register', {account: 'Tài khoản đã tồn tại!'});
-      }
+router.post('/register', function (req, res) {
+    if (req.body.password == req.body.confirmPass) {
+        Account.register(new Account({
+            username: req.body.username,
+            fullname: req.body.fullname,
+            email: req.body.email,
+            phone: req.body.phone,
+            leaderPhone: req.body.leaderPhone
+        }), req.body.password, function (err, account) {
+            if (err) {
+                return res.render('register', {account: 'Tài khoản đã tồn tại!'});
+            }
 
-      passport.authenticate('local')(req, res, function () {
-        req.session.save(function (err) {
-          if (err) {
-            return next(err);
-          }
-          res.redirect('/');
+            passport.authenticate('local')(req, res, function () {
+                req.session.save(function (err) {
+                    if (err) {
+                        return next(err);
+                    }
+                    res.redirect('/');
+                });
+            });
         });
-      });
-    });
-  } else {
-    res.render('register',{error:'Xác nhận mật khẩu không chính xác!'});
-  }
+    } else {
+        res.render('register', {error: 'Xác nhận mật khẩu không chính xác!'});
+    }
 });
 
-router.get('/login', function(req, res) {
-  res.render('login', { user : req.user });
+router.get('/login', function (req, res) {
+    res.render('login', {user: req.user});
 });
 
-router.post('/login', passport.authenticate('local'), function(req, res) {
-  res.redirect('/home');
+// router.post('/login', passport.authenticate('local'), function(req, res) {
+//   res.redirect('/home');
+// });
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/home',
+    failureRedirect: '/login',
+}));
+
+router.get('/logout', function (req, res) {
+    req.logout();
+    res.redirect('/login');
 });
 
-router.get('/logout', function(req, res) {
-  req.logout();
-  res.redirect('/login');
+router.get('/ping', function (req, res) {
+    res.status(200).send("pong!");
 });
 
-router.get('/ping', function(req, res){
-  res.status(200).send("pong!");
+router.get('/viewTable', function (req, res) {
+    if(req.user) {
+        Account.find({}, {_id: 0, salt: 0, hash: 0}, function (err, data) {
+            if (err) throw err;
+
+            // object of all the users
+            res.render('viewTable', {data: JSON.stringify(data)});
+        });
+    }else {
+        res.render('login');
+    }
 });
+router.get('/viewTree', function (req, res) {
+    if (req.user) {
+        Account.find({}, {_id: 0, salt: 0, hash: 0}, function (err, data) {
+            if (err) throw err;
 
-router.get('/viewTable',function (req,res) {
-  Account.find({},{_id:0,salt:0,hash:0}, function(err, data) {
-    if (err) throw err;
-
-    // object of all the users
-    res.render('viewTable',{data:JSON.stringify(data)});
-  });
-});
-router.get('/viewTree',function (req,res) {
-  Account.find({},{_id:0,salt:0,hash:0}, function(err, data) {
-    if (err) throw err;
-
-    // object of all the users
-    res.render('viewTree',{data:JSON.stringify(data)});
-  });
+            // object of all the users
+            res.render('viewTree', {data: JSON.stringify(data)});
+        });
+    }else {
+        res.render('login');
+    }
 });
 module.exports = router;
